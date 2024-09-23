8e6c007d1cef79d654dba260559895a34d64f98d
rarnob1 <rarnob1@myseneca.ca>
2024-09-23 17:13:38 -0400

    update the bat file

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 call venv\scripts\activate
 pytest -s -v -m "sanity or regression" .\test_cases\test_login_admin.py --browser edge -n 3 --html reports/report.html
+pytest -s -v -m "sanity or regression" .\test_cases\test_login_admin.py --browser firefox -n 3 --html reports/report.html
 pause
